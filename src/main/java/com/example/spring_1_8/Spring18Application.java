43d2ab87ff788cb8b2e5ea793de5b5a115084c6c
Yagafarov Arslan <ayagafarov@diasoft.ru>
2025-04-01 14:37:42 +0300

    Initial

---
+++ src/main/java/com/example/spring_1_8/Spring18Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_1_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring18Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring18Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_1_8/Spring18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_1_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring18ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
